16d7a1c84d3b665ca2cb8e2e997d42fef21a6ba7
MishaSabre <106860717+MishaSabre@users.noreply.github.com>
2023-09-17 14:20:43 +0800

    Create Main.java

---
+++ JavaStep/Robot/Main.java
@@ -0,0 +1,55 @@
+package com.mikhailsabre.main;
+
+public class Main {
+    public static void main(String[] args) {}
+
+    public static void moveRobot( Robot robot, int toX, int toY )
+    {
+        if (robot.getX() > toX)
+        {
+            while (robot.getDirection() != Direction.LEFT)
+            {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX)
+            {
+                robot.stepForward();
+            }
+        }
+        else if (robot.getX() < toX)
+        {
+            while (robot.getDirection() != Direction.RIGHT)
+            {
+                robot.turnRight();
+            }
+            while (robot.getX() != toX)
+            {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() > toY)
+        {
+            while (robot.getDirection() != Direction.DOWN)
+            {
+                robot.turnLeft();
+            }
+            while (robot.getY() != toY)
+            {
+                robot.stepForward();
+            }
+        }
+        else if (robot.getY() < toY)
+        {
+            while (robot.getDirection() != Direction.UP)
+            {
+                robot.turnRight();
+            }
+            while (robot.getY() != toY)
+            {
+                robot.stepForward();
+            }
+        }
+    }
+
+}
